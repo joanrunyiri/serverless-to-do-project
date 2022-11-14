c00de511f7bb753406509c602c086f49fab3c315
Joan <joanrnyarua@gmail.com>
2022-11-14 16:07:33 +0300

    initial commit

---
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -0,0 +1,45 @@
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { getAllTodoById, addAttachment } from '../../businessLayer/todos'
+import { UploadUrl } from '../../storageLayer/attachmentUtils'
+
+const bucket_Name = process.env.ATTACHMENT_S3_BUCKET
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+      const todosId = event.pathParameters.todoId
+      // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+      const todos = await getAllTodoById(todosId)
+      todos.attachmentUrl = `https://${bucket_Name}.s3.amazonaws.com/${todosId}`
+      await addAttachment(todos)
+
+      const url = await UploadUrl(todosId)
+
+      return {
+        statusCode: 201,
+
+        body: JSON.stringify({
+          uploadUrl: url
+        })
+      }
+    } catch (error) {
+      return {
+        statusCode: error?.statusCode || 400,
+
+        body: JSON.stringify({
+          message: error?.message || 'error while trying to get url'
+        })
+      }
+    }
+  }
+)
+
+handler.use(httpErrorHandler()).use(
+  cors({
+    credentials: true
+  })
+)
---
+++ client/src/config.ts
@@ -0,0 +1,12 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it [done]
+const apiId = '8rtsdu5xbj'
+export const apiEndpoint =
+  'https://8rtsdu5xbj.execute-api.us-east-1.amazonaws.com/dev'
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map. For example: [done]
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-glv16woiewf3i0qf.us.auth0.com', // Auth0 domain
+  clientId: 'CIfGtcvzQiDv39YyxSLEgHBSTReClgq6', // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
